2cb6d87388a88ed9379ef3493920fe88fb1dfb7f
Tienbruse <tien10a2cvp@gmail.com>
2023-07-21 18:55:55 +0700

    Merge branch 'main' of https://github.com/DuyChien23/ToiDoctor

--- app/src/main/java/com/toier/toidoctor/AppointmentActivity.java
+++ app/src/main/java/com/toier/toidoctor/AppointmentActivity.java
@@ -1,24 +1,32 @@
-package com.toier.toidoctor;
 
-import androidx.appcompat.app.AppCompatActivity;
+package com.toier.toidoctor;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+import com.toier.toidoctor.R.id;
+import com.toier.toidoctor.R.layout;
 
 public class AppointmentActivity extends AppCompatActivity {
-    private Button button;
-    @Override
+    private TextView tvDisplayDateTime;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_appointment);
+        this.setContentView(layout.activity_appointment);
+        this.tvDisplayDateTime = (TextView)this.findViewById(id.tvDisplayDateTime);
+        Intent intent = this.getIntent();
+        if (intent != null) {
+            String selectedDateTime = intent.getStringExtra("SELECTED_DATE_TIME");
+            if (selectedDateTime != null) {
+                this.tvDisplayDateTime.setText("Ngày hẹn khám của bạn: \n" + selectedDateTime);
+            }
+        }
 
-        button = (Button) findViewById(R.id.come_home);
-
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
+        Button button_back_main_activity = findViewById(R.id.tro_ve_trang_chu);
+        button_back_main_activity.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
                 Intent intent = new Intent(AppointmentActivity.this, MainHomeActivity.class);
                 startActivity(intent);
--- app/src/main/java/com/toier/toidoctor/BookingClinicActivity.java
+++ app/src/main/java/com/toier/toidoctor/BookingClinicActivity.java
@@ -1,26 +1,76 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
 package com.toier.toidoctor;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ImageView;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+import com.toier.toidoctor.R.id;
+import com.toier.toidoctor.R.layout;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class BookingClinicActivity extends AppCompatActivity {
-    private ImageView imageView;
-    @Override
+    private Calendar calendar;
+    private SimpleDateFormat dateFormat;
+    private SimpleDateFormat timeFormat;
+    private TextView tvSelectedDateTime;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_booking_clinic);
-
-        imageView = (ImageView) findViewById(R.id.tao_lich_hen);
-        imageView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(BookingClinicActivity.this, AppointmentActivity.class);
-                startActivity(intent);
-            }
+        this.setContentView(layout.activity_booking_clinic);
+        this.calendar = Calendar.getInstance();
+        this.dateFormat = new SimpleDateFormat("dd/MM/yyyy", Locale.getDefault());
+        this.timeFormat = new SimpleDateFormat("HH:mm", Locale.getDefault());
+        Button btnSelectDateTime = (Button)this.findViewById(id.booking);
+        btnSelectDateTime.setOnClickListener((v) -> {
+            this.showDateTimePicker();
         });
+    }
+
+    private void showDateTimePicker() {
+        DatePickerDialog datePickerDialog = new DatePickerDialog(this, (view, year, monthOfYear, dayOfMonth) -> {
+            Calendar selectedCalendar = Calendar.getInstance();
+            selectedCalendar.set(1, year);
+            selectedCalendar.set(2, monthOfYear);
+            selectedCalendar.set(5, dayOfMonth);
+            Calendar todayCalendar = Calendar.getInstance();
+            if (selectedCalendar.before(todayCalendar)) {
+                Toast.makeText(this, "Ngày đã chọn không hợp lệ", Toast.LENGTH_SHORT).show();
+            } else {
+                this.showTimePicker(selectedCalendar);
+            }
+
+        }, this.calendar.get(1), this.calendar.get(2), this.calendar.get(5));
+        datePickerDialog.show();
+    }
+
+    private void showTimePicker(Calendar calendar) {
+        TimePickerDialog timePickerDialog = new TimePickerDialog(this, (view, hourOfDay, minute) -> {
+            int timeCheck = minute - minute / 15 * 15;
+            if (timeCheck <= 7) {
+                minute = minute / 15 * 15;
+            } else {
+                minute = minute / 15 * 15 + 15;
+            }
+
+            calendar.set(11, hourOfDay);
+            calendar.set(12, minute);
+            String selectedDateTime = this.dateFormat.format(calendar.getTime()) + " " + this.timeFormat.format(calendar.getTime());
+            Toast.makeText(this, "Bạn đã chọn: " + selectedDateTime, Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(this, AppointmentActivity.class);
+            intent.putExtra("SELECTED_DATE_TIME", selectedDateTime);
+            this.startActivity(intent);
+        }, calendar.get(11), calendar.get(12), true);
+        timePickerDialog.show();
     }
 }
