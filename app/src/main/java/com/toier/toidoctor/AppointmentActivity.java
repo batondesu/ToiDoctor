0622ff7d707ce9df56d72446e154e960be44f445
batondesu <101453101+batondesu@users.noreply.github.com>
2023-07-26 05:27:52 +0700

    version2
    update controller

--- app/src/main/java/com/toier/toidoctor/AppointmentActivity.java
+++ app/src/main/java/com/toier/toidoctor/AppointmentActivity.java
@@ -3,15 +3,19 @@ package com.toier.toidoctor;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.toier.toidoctor.Controller.BookingController;
 import com.toier.toidoctor.R.id;
 import com.toier.toidoctor.R.layout;
 
 public class AppointmentActivity extends AppCompatActivity {
     private TextView tvDisplayDateTime;
+    private BookingController booking = new BookingController(this);
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +24,8 @@ public class AppointmentActivity extends AppCompatActivity {
         Intent intent = this.getIntent();
         if (intent != null) {
             String selectedDateTime = intent.getStringExtra("SELECTED_DATE_TIME");
+
+            Log.d("QWE", "=>" + selectedDateTime);
             if (selectedDateTime != null) {
                 this.tvDisplayDateTime.setText("Ngày hẹn khám của bạn: \n" + selectedDateTime);
             }
@@ -28,9 +34,51 @@ public class AppointmentActivity extends AppCompatActivity {
         Button button_back_main_activity = findViewById(R.id.tro_ve_trang_chu);
         button_back_main_activity.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-                Intent intent = new Intent(AppointmentActivity.this, MainHomeActivity.class);
-                startActivity(intent);
+                Intent intent3 = new Intent(AppointmentActivity.this, MainHomeActivity.class);
+                startActivity(intent3);
             }
         });
-    }
+
+
+
+
+            String ID = getIntent().getStringExtra("ID_DOCTOR");
+            Log.d("QWE", ID);
+            booking.getDoctorData(ID, new BookingController.OnDoctorDataListener() {
+                @Override
+                public void onDoctorDataReceived(Doctor doctor) {
+                    // Hiển thị tên của Doctor vào TextView
+                    TextView tvDoctorName = findViewById(id.sc_name);
+                    tvDoctorName.setText(doctor.getName());
+
+                    TextView tvDoctorMajor = findViewById(id.sc_major);
+                    tvDoctorMajor.setText(doctor.getMajor());
+
+                    //TextView tvDoctorPatient = findViewById(id.sc);
+                    //tvDoctorPatient.setText(String.format("+%d",doctor.getPatient()));
+
+                    TextView tvDoctorReview = findViewById(id.sc_review);
+                    tvDoctorReview.setText(String.format("%d Đánh giá",doctor.getReview()));
+
+                    TextView tvDoctorRate = findViewById(id.sc_rate);
+                    tvDoctorRate.setText(String.format("%.1f",doctor.getRate()));
+
+                    TextView tvDoctorAbout = findViewById(id.about_doctor);
+                    //tvDoctorAbout.setText(doctor.getAbout_doctor());
+
+                    TextView tvDoctorAddress = findViewById(id.address);
+                    //tvDoctorName.setText(doctor.getAddress());
+
+                    TextView tvDoctorHospital = findViewById(id.hospital_name);
+                    //tvDoctorName.setText(doctor.getHospital_name());
+
+                }
+
+                @Override
+                public void onDoctorDataError(String errorMessage) {
+                    // Xử lý lỗi nếu có
+                }
+            });
+        }
+
 }
---
+++ app/src/main/java/com/toier/toidoctor/Controller/MainHomeController.java
@@ -0,0 +1,121 @@
+package com.toier.toidoctor.Controller;
+
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.toier.toidoctor.BookingClinicActivity;
+import com.toier.toidoctor.Doctor;
+import com.toier.toidoctor.ListDoctorActivity;
+import com.toier.toidoctor.MainHomeActivity;
+import com.toier.toidoctor.PatientProfileActivity;
+import com.toier.toidoctor.R;
+
+import java.util.ArrayList;
+
+public class MainHomeController {
+
+    private Context context;
+
+    private ArrayList<Doctor> listDoctor1;
+
+    public MainHomeController(Context context) {
+        this.context = context;
+        this.listDoctor1 = new ArrayList<>();
+    }
+
+    public void setData(ListView listView) {
+        // Tạo Adapter và thiết lập cho ListView
+        //ArrayList<Doctor> listDoctor = new ArrayList<>();
+        //fetchDoctorsFromFirestore();
+        Log.d("ABC", String.format("Size: %d",listDoctor1.size()) );
+        for(int i = 0 ; i < listDoctor1.size() ; ++i) {
+            Log.d("ABC", listDoctor1.get(i).getName().toString() );
+        }
+
+        /*ArrayList<Doctor> doctorList1 = new ArrayList<>();
+        doctorList1.add(new Doctor("John Doe", "Cardiologist", 1,1,"","","","",0,0));
+        doctorList1.add(new Doctor("Jane Smith", "Pediatrician",1,1,"","","","",0,0));*/
+
+        ListDoctor listDoctorAdapter = new ListDoctor(context, listDoctor1);
+        listView.setAdapter(listDoctorAdapter);
+    }
+
+    // Hàm để lấy dữ liệu Doctor từ Firestore và thêm vào listDoctor
+    public void fetchDoctorsFromFirestore(ListView listView) {
+
+        // Truy cập vào collection "doctors" trong Firestore
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        CollectionReference doctorsRef = db.collection("Doctors");
+
+        doctorsRef.get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+
+                if (task.isSuccessful()) {
+                    QuerySnapshot querySnapshot = task.getResult();
+                    if (querySnapshot != null && !querySnapshot.isEmpty()) {
+                        //listDoctor.clear(); // Xóa danh sách hiện tại để tránh trùng lặp
+                        for (DocumentSnapshot document : querySnapshot.getDocuments()) {
+                            //Log.d("XXX" , "dm e DangLeHai"  );
+
+
+                            // Lấy dữ liệu từ Firestore và tạo đối tượng Doctor
+                            Doctor doctor = new Doctor();
+                            doctor.setDoctor_ID(document.get("ID").toString());
+                            doctor.setAbout_doctor(document.get("about_doctor").toString());
+                            doctor.setName(document.get("name").toString());
+                            doctor.setMajor(document.get("major").toString());
+                            double rate = document.getDouble("rate");
+                            doctor.setRate(rate);
+                            long res = document.getLong("review");
+                            doctor.setReview((int) res);
+                            long res1 = document.getLong("exp");
+                            doctor.setExp((int) res1);
+                            long res2 = document.getLong("patient");
+                            doctor.setPatient((int) res2);
+
+
+                            // Thêm đối tượng Doctor vào listDoctor
+                            listDoctor1.add(doctor);
+                            //Log.d("ABC", String.format("Size: %d",listDoctor.size()) );
+                        }
+                        //Log.d("ABC", String.format("Size: %d",listDoctor1.size()) );
+                        ListDoctor listDoctorAdapter = new ListDoctor(context, listDoctor1);
+                        listView.setAdapter(listDoctorAdapter);
+                        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                            @Override
+                            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                                Doctor doctor  = (Doctor) listDoctorAdapter.getItem(position);
+                                        Intent intent = new Intent(context, BookingClinicActivity.class);
+                                intent.putExtra("KEY_VALUE", doctor.getDoctor_ID());
+                                context.startActivity(intent);
+                            }
+                        });
+                    } else {
+                        // Không có dữ liệu trong collection "doctors"
+                    }
+                } else {
+                    // Xử lý lỗi khi lấy dữ liệu từ Firestore
+                }
+            }
+        });
+    }
+}
