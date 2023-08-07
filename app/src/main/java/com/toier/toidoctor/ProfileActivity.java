448fe997296fabec739dd98076a1d96d6ab88afc
ledang <haidangle1907@gmail.com>
2023-08-07 21:43:30 +0700

    Commit new Profile

--- app/src/main/java/com/toier/toidoctor/ProfileActivity.java
+++ app/src/main/java/com/toier/toidoctor/ProfileActivity.java
@@ -1,16 +1,49 @@
 package com.toier.toidoctor;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.Timestamp;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.toier.toidoctor.Controller.patientMedicalRecord;
+import com.toier.toidoctor.Controller.patientprofile;
+
+import org.w3c.dom.Text;
+
+import java.util.Date;
+
 public class ProfileActivity extends AppCompatActivity {
+    private void setButton(String id, String time, String detail) {
+        Button btnTest =  new Button(this);
+        btnTest = patientMedicalRecord.createButton(this, btnTest, id, time);
+        LinearLayout layout = (LinearLayout) findViewById(R.id.updatedDate);
+        layout.addView(btnTest);
+        btnTest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //btnTest.setBackgroundColor(getResources().getColor(android.R.color.holo_blue_bright)); // Set the background color to green
+                TextView result = (TextView) findViewById(R.id.resultdetail);
+                result.setText(detail);
+
+            }
+        });
+
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,6 +61,52 @@ public class ProfileActivity extends AppCompatActivity {
         //patientDetail.setText(getIntent().getStringExtra("content"));
         /*ViewGroup.LayoutParams layoutParams = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
         patientName.setLayoutParams(layoutParams);*/
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+//      Lấy thông tin bệnh nhân
+        db.collection("Patients")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+
+
+                    @Override
+                    public void onComplete  (@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d("ABC", document.getId() + " => " + document.getData());
+                                String user_id = String.valueOf(document.getData().get("User"));
+                                if (user_id.equals(getIntent().getStringExtra("id"))) {
+                                    db.collection("Patients/" + document.getId() + "/medicalRecord")
+                                            .get()
+                                            .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                                                @Override
+                                                public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                                    if (task.isSuccessful()) {
+                                                        for (QueryDocumentSnapshot document : task.getResult()) {
+                                                            Log.d("ABC", document.getId() + " => " + document.getData());
+                                                            Timestamp timestamp = (Timestamp) document.getData().get("created_at");
+                                                            Date date = timestamp.toDate();
+                                                            String time = date.getDate() + "/"  + String.valueOf(date.getMonth()+1) + "/" +  String.valueOf(1900 + date.getYear());
+                                                            setButton(document.getId(), time, String.valueOf(document.getData().get("detail")));
+                                                        }
+                                                    } else {
+                                                        Log.w("ABC", "Error getting documents.", task.getException());
+                                                    }
+                                                }
+                                            });
+                                    //setButton(document.getId(), String.valueOf(document.getData().get("name")), String.valueOf(document.getData().get("phoneNumber")));
+                                    //setName( String.valueOf(document.getData().get("name")));
+                                    //setPhoneNumber( String.valueOf(document.getData().get("phoneNumber")));
+                                    //setButton(document.getId(), getName(), getPhoneNumber());
+
+                                }
+                            }
+                        } else {
+                            Log.w("ABC", "Error getting documents.", task.getException());
+                        }
+                    }
+                });
 
 
         btnCall.setOnClickListener(new View.OnClickListener() {
@@ -47,6 +126,8 @@ public class ProfileActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+
 
     }
 }
